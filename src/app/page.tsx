e902b55175890573c1cd32abb7805c9384c04857
iamasink <iamasink@proton.me>
2023-06-18 04:06:00 +0100

    minor changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,27 +6,37 @@ export default function Home() {
 	return (
 		<main className="flex min-h-screen  flex-col items-center justify-between p-1 pt-24">
 			<div className="mb-32 grid text-center lg:mb-0 lg:text-left">
-				<div className="p-x-10 flex items-center text-center">
+				<div className="p-x-10 items-top flex text-center">
 					<Image
 						src="/pfp.jpg"
 						alt="image"
 						draggable={false}
-						width={100}
+						width={200}
 						height={0}
 						style={{
 							borderRadius: "15%",
-							border: "2px solid #ffffff",
+							border: "3px solid #ffffff",
 						}}
 						className="m-4"
 						// sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
 					></Image>
-					<div className="group flex">
+					<div className="group">
 						<h1
-							className={`m-0 max-w-[30ch] text-5xl`}
-							style={{ letterSpacing: 1.5, zIndex: "1" }}
+							className={`m-0 max-w-[30ch] pt-9 text-5xl`}
+							style={{ letterSpacing: 1.5 }}
 						>
 							iamasink
 						</h1>
+						<p className="m-0 pt-9 text-2xl">Lily </p>
+						<p>
+							<span className="border border-transparent text-white">
+								she/her
+							</span>{" "}
+							<span className="text-pink-500 transition-all hover:bg-pink-500 hover:text-pink-100">
+								&lt;3
+							</span>
+						</p>
+
 						{/* <h1
 							className={`m-0 max-w-[30ch] text-3xl transition-all group-hover:opacity-0`}
 							aria-hidden={true}
@@ -50,26 +60,22 @@ export default function Home() {
 						src="/github.svg"
 						alt="GitHub"
 						text="GitHub"
-					/>
-				</div>
-				<div className="grid grid-cols-3">
-					<SocialLinkSmall
+					/>{" "}
+					<SocialLink
 						href="https://twitter.com/iamasink"
 						src="/twitter.svg"
 						alt="Twitter"
+						text="Twitter"
 					/>
-
-					<SocialLinkSmall
+					<SocialLink
 						href="https://steamcommunity.com/id/iamasink/"
 						src="/steam.svg"
 						alt="Steam"
+						text="Steam"
 					/>
-
-					<SocialLinkSmall
-						href="https://iamas.ink/discord"
-						src="/discord.svg"
-						alt="Discord Server"
-					/>
+				</div>
+				<div className="grid grid-cols-3">
+					{/* small ones go here */}
 				</div>
 			</div>
 		</main>
--- src/components/SocialLink.tsx
+++ src/components/SocialLink.tsx
@@ -11,24 +11,26 @@ const SocialLink: React.FC<SocialLinkProps> = ({ href, src, alt, text }) => {
 	return (
 		<a
 			href={href}
-			className="group m-1 flex rounded-lg border border-transparent px-4 py-1 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 lg:px-5 lg:py-5"
+			className="group m-1 flex rounded-lg border border-transparent px-5 py-1 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 lg:px-5 lg:py-5"
 			target="_blank"
 			rel="noopener noreferrer"
 		>
-			<div className="flex aspect-square items-center border-gray-100 p-1 align-middle transition-transform group-hover:translate-x-3">
-				<Image
-					className="inline-block object-center align-middle"
-					src={src}
-					alt={alt}
-					height={40}
-					width={40}
-				/>
-			</div>
-			<div className="flex items-center border-gray-100 p-1 align-middle transition-transform group-hover:translate-x-1">
-				{" "}
-				<h2 className="translate-x-3 text-3xl font-semibold transition-transform group-hover:translate-x-3 lg:text-4xl">
-					{text}
-				</h2>
+			<div className="flex px-2">
+				<div className="flex aspect-square items-center border-gray-100 p-1 align-middle transition-transform group-hover:translate-x-3">
+					<Image
+						className="inline-block object-center align-middle"
+						src={src}
+						alt={alt}
+						height={40}
+						width={40}
+					/>
+				</div>
+				<div className="flex items-center border-gray-100 p-1 pr-4 align-middle transition-transform group-hover:translate-x-1">
+					{" "}
+					<h2 className="translate-x-3 px-3 text-3xl font-semibold transition-transform group-hover:translate-x-3 lg:text-4xl">
+						{text}
+					</h2>
+				</div>
 			</div>
 		</a>
 	);
