5cb44273c78a18c45ceb5cde6d4415165d113cb9
iamasink <iamasink@proton.me>
2024-04-04 01:08:35 +0100

    b

--- src/components/IamasinkTitle.tsx
+++ src/components/IamasinkTitle.tsx
@@ -3,12 +3,12 @@
 import Link from "next/link";
 import { motion } from "framer-motion";
 
-const move1 = {
-	initial: { x: 0, opacity: 1, delay: 0, color: "#transparent" },
+const hovermovedot = {
+	initial: { x: 0, opacity: 1, delay: 0, color: "transparent" },
 	animate: { x: -70, opacity: 1, delay: 0, color: "#00ff00" },
 };
-const move2 = {
-	initial: { x: 0, opacity: 1, delay: 0, color: "#transparent" },
+const hovermoveink = {
+	initial: { x: 0, opacity: 1, delay: 0, color: "transparent" },
 	animate: { x: 4, opacity: 1, delay: 0, color: "#ff0000" },
 };
 
@@ -29,27 +29,27 @@ const IamasinkTitle: React.FC = ({}) => {
 			<motion.div
 				initial={{ x: 4, opacity: 1 }}
 				animate={{ x: 0, opacity: 1 }}
+				whileHover={"initial"}
 				transition={{ delay: 1, ease: "easeInOut", duration: 0.15 }}
 			>
-				<motion.h1
-					variants={move2}
+				<h1
 					className={`text-5xl text-transparent transition-all duration-300  `}
 					style={{}}
 				>
 					ink
-				</motion.h1>
+				</h1>
 			</motion.div>
 			<motion.div
 				initial={{ x: -70, opacity: 1 }}
 				animate={{ x: 0, opacity: 1 }}
+				whileHover={"initial"}
 				transition={{ delay: 1, ease: "easeInOut", duration: 0.3 }}
 			>
-				<motion.h1
-					variants={move1}
-					className={`m-0 max-w-[30ch] text-5xl text-purple-500 transition-all group-hover:text-transparent`}
+				<h1
+					className={`m-0 max-w-[30ch] text-5xl text-transparent transition-all `}
 				>
 					.
-				</motion.h1>
+				</h1>
 			</motion.div>
 		</motion.div>
 	);
