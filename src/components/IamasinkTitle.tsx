93e2c4b4e7541aeafed9fec565569b65575d917c
iamasink <iamasink@proton.me>
2024-04-04 01:47:22 +0100

    c

--- src/components/IamasinkTitle.tsx
+++ src/components/IamasinkTitle.tsx
@@ -1,57 +1,26 @@
-"use client";
-
 import Link from "next/link";
-import { motion } from "framer-motion";
-
-const hovermovedot = {
-	initial: { x: 0, opacity: 1, delay: 0, color: "transparent" },
-	animate: { x: -70, opacity: 1, delay: 0, color: "#00ff00" },
-};
-const hovermoveink = {
-	initial: { x: 0, opacity: 1, delay: 0, color: "transparent" },
-	animate: { x: 4, opacity: 1, delay: 0, color: "#ff0000" },
-};
 
 const IamasinkTitle: React.FC = ({}) => {
 	return (
-		<motion.div
-			initial="initial"
-			animate="initial"
-			whileHover="animate"
-			className="animated-background group m-0 mt-9 inline-flex max-w-[30ch] bg-gradient-to-r from-white to-pink-700 bg-[length:400px_200px] bg-clip-text hover:text-purple-500"
-		>
+		<div className="animated-background group m-0 mt-9 inline-flex max-w-[30ch] bg-gradient-to-r from-white to-pink-700 bg-[length:400px_200px] bg-clip-text hover:text-purple-500">
 			<h1
 				className={` text-5xl text-transparent transition-all  `}
 				style={{}}
 			>
 				iamas
 			</h1>
-			<motion.div
-				initial={{ x: 4, opacity: 1 }}
-				animate={{ x: 0, opacity: 1 }}
-				whileHover={"initial"}
-				transition={{ delay: 1, ease: "easeInOut", duration: 0.15 }}
+			<h1
+				className={`text-5xl text-transparent transition-all duration-300 group-hover:translate-x-1 `}
+				style={{}}
 			>
-				<h1
-					className={`text-5xl text-transparent transition-all duration-300  `}
-					style={{}}
-				>
-					ink
-				</h1>
-			</motion.div>
-			<motion.div
-				initial={{ x: -70, opacity: 1 }}
-				animate={{ x: 0, opacity: 1 }}
-				whileHover={"initial"}
-				transition={{ delay: 1, ease: "easeInOut", duration: 0.3 }}
+				ink
+			</h1>
+			<h2
+				className={`m-0 max-w-[30ch] text-5xl text-purple-500 transition-all group-hover:-translate-x-[70px] group-hover:text-transparent`}
 			>
-				<h1
-					className={`m-0 max-w-[30ch] text-5xl text-transparent transition-all `}
-				>
-					.
-				</h1>
-			</motion.div>
-		</motion.div>
+				.
+			</h2>
+		</div>
 	);
 };
 
