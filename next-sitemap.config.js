08b9af0d25b31005a2a8746f98c28459fbb5e78a
iamasink <iamasink@proton.me>
2024-04-24 21:26:26 +0100

    fix sitemap config

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,7 +2,4 @@
 module.exports = {
     siteUrl: process.env.SITE_URL || 'https://iamas.ink',
     generateRobotsTxt: true, // (optional)
-    additionalSitemaps: [
-        'https://q.iamas.ink/sitemap.xml'
-    ],
 }
