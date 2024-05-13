3c74948d13731e5f1bcb72c0addac7f72422018d
iamasink <iamasink@proton.me>
2024-05-13 21:09:38 +0100

    remove log lol

--- src/components/DiscordStatus.tsx
+++ src/components/DiscordStatus.tsx
@@ -12,7 +12,7 @@ const DiscordStatus: React.FC = ({ }) => {
         // client-side-only code
         setInterval(() => {
             reloadImg(`https://lanyard.cnrad.dev/api/303267459824353280?showDisplayName=true&hideActivity=whenNotUsed&showDisplayName=true&theme=dark&bg=bg=809ecf00&hideProfile=true&hideTimestamp=false&${Date.now()}`)
-            console.log("Hello, world!")
+            // console.log("Hello, world!")
         }, 15 * 1000)
     }
     return (
