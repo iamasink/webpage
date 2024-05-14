aa3f6ac04fed5639cbf2697ad664c128918cfa82
iamasink <iamasink@proton.me>
2024-05-14 03:17:38 +0100

    change description and add favicon and site info to head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ const robotoMono = Roboto_Mono({
 
 export const metadata = {
 	title: "iamasink",
-	description: "iamasink homepage",
+	description: "lillie's site",
 };
 
 export default function RootLayout({
@@ -31,6 +31,14 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
+			<head>
+				<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+				<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+				<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+				<link rel="manifest" href="/site.webmanifest" />
+				<meta name="msapplication-TileColor" content="#7f0086" />
+				<meta name="darkreader-lock" />
+			</head>
 			<body className={inter.className}>{children}</body>
 		</html>
 	);
