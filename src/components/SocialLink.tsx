773fedda94dbd8ae6a8ccbec46ca12d5283778b9
iamasink <iamasink@proton.me>
2024-05-15 20:56:48 +0100

    fix text cutoff

--- src/components/SocialLink.tsx
+++ src/components/SocialLink.tsx
@@ -30,7 +30,7 @@ const SocialLink: React.FC<SocialLinkProps> = ({
 			rel={rel}
 			draggable={false}
 		>
-			<div className="flex px-2">
+			<div className="flex h-full px-2 ">
 				<div className="flex aspect-square items-center border-gray-100 p-1 align-middle transition-transform group-hover:translate-x-3">
 					<Image
 						className="inline-block object-center align-middle"
@@ -40,9 +40,9 @@ const SocialLink: React.FC<SocialLinkProps> = ({
 						width={40}
 					/>
 				</div>
-				<div className="flex items-center border-gray-100 p-1 pr-4 align-middle transition-transform group-hover:translate-x-1">
+				<div className="flex items-center border-gray-100 pl-1 pr-4 align-middle transition-transform group-hover:translate-x-1">
 					{" "}
-					<h2 className="translate-x-3 bg-white bg-clip-text px-3 text-3xl font-semibold text-transparent text-opacity-100 transition-all duration-500 group-hover:translate-x-3 group-hover:bg-pink-200 lg:text-4xl">
+					<h2 className=" py-1 translate-x-3 bg-white bg-clip-text px-3 text-3xl font-semibold text-transparent text-opacity-100 transition-all duration-500 group-hover:translate-x-3 group-hover:bg-pink-200 lg:text-4xl">
 						{text}
 					</h2>
 				</div>
