db55984e302f9173ab1629803e6b3bd9cc833298
iamasink <iamasink@proton.me>
2024-05-15 20:56:30 +0100

    resize and remove logging

--- src/components/DiscordStatus.tsx
+++ src/components/DiscordStatus.tsx
@@ -2,10 +2,9 @@
 import Link from "next/link";
 import { motion } from "framer-motion";
 
-const url = "https:///lanyard-profile-readme-rho.vercel.app//api/303267459824353280?showDisplayName=true&hideActivity=whenNotUsed&showDisplayName=true&theme=dark&bg=809ecf00&hideProfile=true&hideTimestamp=true&"
+const url = "https:///lanyard-profile-readme-rho.vercel.app/api/303267459824353280?showDisplayName=true&hideActivity=whenNotUsed&showDisplayName=true&theme=dark&bg=809ecf00&hideProfile=true&hideTimestamp=true&"
 
 const DiscordStatus: React.FC = ({ }) => {
-
     async function reloadImg(url: string) {
         await fetch(url, { cache: 'reload', mode: 'no-cors' })
         const image: any = document.getElementById("discordstatus")
@@ -16,7 +15,7 @@ const DiscordStatus: React.FC = ({ }) => {
 
         setInterval(() => {
             // reloadImg(`https://https://lanyard.cnrad.dev/api/303267459824353280?showDisplayName=true&hideActivity=whenNotUsed&showDisplayName=true&theme=dark&bg=bg=809ecf00&hideProfile=true&hideTimestamp=false&${Date.now()}`)
-            console.log("Hello, world!")
+            // console.log("Hello, world!")
             reloadImg(`${url + Date.now()}`)
         }, 15 * 1000)
     }
@@ -26,7 +25,7 @@ const DiscordStatus: React.FC = ({ }) => {
             animate={{ y: 0, opacity: 1 }}
             transition={{ ease: "easeInOut", duration: 3 }}
         >
-            <div className="h-[130px] w-[410px]">
+            <div className="-mt-5 h-[110px] w-[410px]">
                 <img suppressHydrationWarning id="discordstatus" draggable="false" src={url} alt="Discord Presence" ></img>
             </div>
         </motion.div>
