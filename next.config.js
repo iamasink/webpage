f3491bad5fce50dc9ae39f7a313bec7c50161521
iamasink <iamasink@proton.me>
2024-05-15 20:56:08 +0100

    unused rewrite

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
+    // async rewrites() {
+    //     return [
+    //         {
+    //             source: "/html/:slug",
+    //             destination: "/html/:slug.html",
+    //         }
+    //     ]
+    // },
 }
 
 module.exports = nextConfig
