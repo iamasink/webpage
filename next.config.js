6663431661c25057a1f02719387958cad66a8dc6
iamasink <iamasink@proton.me>
2024-06-19 20:16:17 +0100

    add headers

--- next.config.js
+++ next.config.js
@@ -9,6 +9,38 @@ const nextConfig = {
     //         }
     //     ]
     // },
+    // async headers() {
+    //     return [
+    //         {
+    //             // Apply these headers to all routes in your application.
+    //             source: "/(.*)",
+    //             headers: securityHeaders,
+    //         },
+    //     ];
+    // },
 }
 
 module.exports = nextConfig
+
+const securityHeaders = [
+    {
+        key: "Permissions-Policy",
+        value: "camera=(), microphone=(), geolocation=(), browsing-topics=()",
+    },
+    {
+        key: "X-DNS-Prefetch-Control",
+        value: "on",
+    },
+    {
+        key: "X-Frame-Options",
+        value: "SAMEORIGIN",
+    },
+    {
+        key: "X-XSS-Protection",
+        value: "1; mode=block",
+    },
+    {
+        key: "x-content-type-options",
+        value: "nosniff",
+    },
+];
