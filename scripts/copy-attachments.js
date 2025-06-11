b8a73f6425a3a5b5422585c2b1478bfd0b49c1b7
iamasink <iamasink@proton.me>
2025-06-11 03:08:13 +0100

    change attachments path back

--- scripts/copy-attachments.js
+++ scripts/copy-attachments.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const ncp = require('ncp').ncp;
 
-const ATTACHMENTS_FOLDER_PATH = "public/blog/Attachments"
+const ATTACHMENTS_FOLDER_PATH = "public/Attachments"
 
 const source = path.join(__dirname, "..", 'content/blog/Attachments');
 const destination = path.join(__dirname, "..", ATTACHMENTS_FOLDER_PATH);
