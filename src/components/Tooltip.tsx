4746243fda97a0a0029260724bf7f036b411776f
iamasink <iamasink@proton.me>
2025-06-11 02:54:34 +0100

    remove unnecessary span

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -9,11 +9,9 @@ const Tooltip: React.FC<TooltipProps> = ({ children, message }) => {
 	return (
 		<span className="group relative inline-block">
 			{children}
-			<span className="">
 				<span className="absolute left-1/2 top-8 -translate-x-1/2 scale-0 transform whitespace-nowrap rounded bg-gradient-to-br from-pink-950 to-pink-500 bg-[length:400px_200px] p-2 text-xs text-white opacity-0 transition-all group-hover:scale-100 group-hover:opacity-100">
 					{message}
 				</span>
-			</span>
 		</span>
 	)
 }
