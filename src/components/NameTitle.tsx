5a4f6e68285a6c8ae1dd403bd868023d9ddeab12
iamasink <iamasink@proton.me>
2025-09-22 20:41:08 +0100

    username hover

--- src/components/NameTitle.tsx
+++ src/components/NameTitle.tsx
@@ -1,22 +1,24 @@
 import Link from "next/link"
+import "./NameTitle.css" // make sure to import the CSS
+
 
 const NameTitle: React.FC = ({ }) => {
 	return (
-		<div className="text-5xl bg-clip-text animated-background group m-0 mt-9 inline-flex max-w-[30ch] bg-gradient-to-r from-white to-pink-700 bg-[length:400px_200px] group-hover:text-purple-500">
-			<span
-				className={`text-transparent `}
-				style={{}}
-			>
-				iamasink
-			</span>
-			<span
+		<div className="namecontainer">
+			<div className="text-5xl bg-clip-text animated-background group m-0 mt-9 inline-flex max-w-[30ch] bg-gradient-to-r from-white to-pink-700 bg-[length:400px_200px] group-hover:text-purple-500">
+				<span className={`name text-transparent`}>
+					<span>i</span><span>am</span><span>a</span><span>sink</span>
+				</span>
+
+				{/* <span
 				className={` select-none text-purple-800 group-hover:opacity-75 opacity-0 transition-opacity -translate-x-[73px] align-bottom`}
 				style={{
 					transition: "0.5s ease-in-out",
 				}}
 			>
 				.
-			</span>
+			</span> */}
+			</div>
 		</div>
 	)
 }
